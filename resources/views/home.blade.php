1b26791f29f85e1d30d72d46a0f15a6c8eaa702b
AbdullahCassim <abdullahvip090@gmail.com>
2025-11-10 20:37:06 +0530

    Refactor home view to include header and navbar components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Student Managment System</title>
-</head>
-<body>
-    <h1>HELO</h1>
+@include('components.head')
+@include('components.navbar')
+   
 </body>
 </html>
